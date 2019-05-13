cea92d518028aae0d9a987f769f2693e8d63179d
shariff6 <shariff.mohammed.a@gmail.com>
2019-05-13 09:45:44 +0300

    User is Able To Input Encryted Phrase and Return Decrypted, User Can Choose To Exit Application

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,19 +6,29 @@ public class App {
 
         Console myConsole = System.console();
 
-        System.out.println("Welcome, what would you like to do, Encryt a Phrase, Decrypt A Phrase or Exit");
+        System.out.println("Welcome, what would you like to do, Encrypt a Phrase, Decrypt A Phrase or Exit");
         String userChoice = myConsole.readLine();
-
-        if (userChoice.equals("Encrypt")) {
-            System.out.println("Enter phrase To Encrypt");
-            String userPhrase = myConsole.readLine();
-            System.out.println("Enter Encryption Key");
-            Integer encodingKey = Integer.parseInt(myConsole.readLine());
-            CeaserCipher userInput = new CeaserCipher();
-            String encodedString = userInput.encodeString(userPhrase, encodingKey);
-            System.out.println(encodedString);
-        }
-
-
+        boolean programRunning = true;
+      while (programRunning) {
+          if (userChoice.toUpperCase().equals("ENCRYPT")) {
+              System.out.println("Enter phrase To Encrypt");
+              String userPhrase = myConsole.readLine();
+              System.out.println("Enter Encryption Key");
+              Integer encodingKey = Integer.parseInt(myConsole.readLine());
+              CeaserCipher userInput = new CeaserCipher();
+              String encodedString = userInput.encodeString(userPhrase, encodingKey);
+              System.out.println(encodedString);
+          } else if (userChoice.toUpperCase().equals("DECRYPT")) {
+              System.out.println("Enter phrase To Decrypt");
+              String userPhrase = myConsole.readLine();
+              System.out.println("Enter Decryption Key");
+              Integer encodingKey = Integer.parseInt(myConsole.readLine());
+              CeaserCipher userInput = new CeaserCipher();
+              String decodedString = userInput.decryptString(userPhrase, encodingKey);
+              System.out.println(decodedString);
+          } else if (userChoice.toUpperCase().equals("EXIT")) {
+              programRunning = false;
+          }
+      }
     }
 }
